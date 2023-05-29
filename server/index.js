1cd59c83a899513a66c4a90cee91a1e6717e94b1
Duong Pham <t0phtr00@students.oamk.fi>
2023-05-29 15:34:45 +0300

    Connect To MongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import * as dotenv from "dotenv"
 import cors from "cors"
 
+import connectDB from "./mongodb/connect.js"
+
 dotenv.config()
 
 const app = express()
@@ -12,10 +14,15 @@ app.get("/", async (req, res) => {
   res.send("Hello World!")
 })
 
-const startSever = async () => {
-  app.listen(8080, () => {
-    console.log("Server has started on port http://localhost:8080")
-  })
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL)
+    app.listen(8080, () =>
+      console.log("Server started on port http://localhost:8080/")
+    )
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-startSever()
+startServer()
